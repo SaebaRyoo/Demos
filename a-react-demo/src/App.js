ee6543e2854d7e7f5506db5f1851534efdc9e4a8
朱青源 <yuanddmail@163.com>
2023-02-20 11:19:33 +0800

    update

--- a-react-demo/src/App.js
+++ a-react-demo/src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
-import { sum } from '@lxnxbnq/utils';
 
 function App() {
-  console.log(sum(1,2,3))
+  const [value, setV] = useState(null)
+  const handleChange = (e) => {setV(e.target.value)}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type="text" value={value} onChange={handleChange} />
+      <p>{value}</p>
     </div>
   );
 }
